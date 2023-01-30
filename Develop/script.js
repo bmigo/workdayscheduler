2baa13d15b22a0a14e2897b107517758af9920b3
bmigo <berniecmigo@gmail.com>
2023-01-30 15:31:32 -0500

    js

---
+++ Develop/script.js
@@ -0,0 +1,43 @@
+var now = moment().format("dddd, MMMM Do YYYY");
+$("#currentDay").html(now);
+var currentHour = moment().hour();
+
+$(".time-block").each(function() {
+    var timeBlock = $(this).attr("id").split("hour")[1];
+
+    if (timeBlock < currentHour) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+    }
+    else if (timeBlock === currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+
+    }
+});
+
+$(".saveBtn").on("click", function () {
+    var task = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, task);
+});
+
+$("hour09 .description").val(localStorage.getItem("hour09"));
+$("hour10 .description").val(localStorage.getItem("hour10"));
+$("hour11 .description").val(localStorage.getItem("hour11"));
+$("hour12 .description").val(localStorage.getItem("hour12"));
+$("hour13 .description").val(localStorage.getItem("hour13"));
+$("hour14 .description").val(localStorage.getItem("hour14"));
+$("hour15 .description").val(localStorage.getItem("hour15"));
+$("hour16 .description").val(localStorage.getItem("hour16"));
+$("hour17 .description").val(localStorage.getItem("hour17"));
+
+
+
