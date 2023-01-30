184b6bde9b36791565cba419556ef5cdc42207ca
bmigo <berniecmigo@gmail.com>
2023-01-30 16:07:51 -0500

    askBCS

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,14 @@
 var now = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").html(now);
 var currentHour = moment().hour();
-
+var elements = $(".time-block");
+console.log(elements);
 $(".time-block").each(function() {
-    var timeBlock = $(this).attr("id").split("hour")[1];
-
-    if (timeBlock < currentHour) {
+    var timeBlock1 = $(this).attr("id");
+    console.log(timeBlock1);
+    var timeBlock = timeBlock1.substr(-2);
+    console.log(timeBlock);
+/*     if (timeBlock < currentHour) {
         $(this).removeClass("future");
         $(this).removeClass("present");
         $(this).addClass("past");
@@ -20,7 +23,7 @@ $(".time-block").each(function() {
         $(this).removeClass("past");
         $(this).addClass("future");
 
-    }
+    } */
 });
 
 $(".saveBtn").on("click", function () {
@@ -29,15 +32,15 @@ $(".saveBtn").on("click", function () {
     localStorage.setItem(time, task);
 });
 
-$("hour09 .description").val(localStorage.getItem("hour09"));
-$("hour10 .description").val(localStorage.getItem("hour10"));
-$("hour11 .description").val(localStorage.getItem("hour11"));
-$("hour12 .description").val(localStorage.getItem("hour12"));
-$("hour13 .description").val(localStorage.getItem("hour13"));
-$("hour14 .description").val(localStorage.getItem("hour14"));
-$("hour15 .description").val(localStorage.getItem("hour15"));
-$("hour16 .description").val(localStorage.getItem("hour16"));
-$("hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour09 .description").val(localStorage.getItem("hour09"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
 
 
